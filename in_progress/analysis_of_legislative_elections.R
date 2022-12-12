eee5f13686485cef4d176aad0739c8c42c4d3cdc
Guilherme Vieira <gvmmpg@gmail.com>
2022-12-12 12:13:07 -0300

    add semicircle parliament

--- in_progress/analysis_of_legislative_elections.R
+++ in_progress/analysis_of_legislative_elections.R
@@ -1,5 +1,7 @@
 # analysis of legislative elections
 
+# TO IMPROVE: require smooth the plot, there are empty spaces
+
 
 # SET MAIN DIRECTORY
 setwd("C:/Users/guima/Desktop/data_science/Projetos/elections_analytics_-_minas_gerais/in_progress")
@@ -30,15 +32,20 @@ colnames(by_federals) <- columns_names
 
 parties_colores <- c(
   "PMN"="tomato4",
+  "PCO"="coral4",
+  "PSTU"="firebrick4",
+  "PCB"="brown4",
   "PCdoB"="red4",
   "PSOL"="orange",
   "PV"="green",
   "PT"="red",
   "PDT"="firebrick",
   "PSB"="gold",
+  "Rede"="darkseagreen",
   "Solidariedade"="darkorange4",
   "MDB"="grey",
   "PSD"="aquamarine1",
+  "Cidadania"="darkorchid1",
   "PSDB"="royalblue",
   "AVANTE"="brown1",
   "PRP"="slategray1",
@@ -50,9 +57,11 @@ parties_colores <- c(
   "PHS"="cadetblue2",
   "PSC"="cyan",
   "União Brasil"="deepskyblue",
+  "NOVO"="darkorange",
   "PL"="darkblue",
   "PTB"="darkslategrey",
-  "Republicanos"="mediumblue"
+  "Republicanos"="mediumblue",
+  "PSDC"="dodgerblue"
 )
 
 
@@ -63,13 +72,23 @@ parties_colores <- c(
 
 reduce_variables <- function(df) {
   
-  df <- df %>%
-    select(ANO_ELEICAO, NR_TURNO, SG_UF, NM_UE, CD_MUNICIPIO,
-           NM_MUNICIPIO, DS_CARGO, NM_CANDIDATO, NM_URNA_CANDIDATO,
-           SG_PARTIDO, NM_PARTIDO, QT_VOTOS_NOMINAIS_VALIDOS,
-           DS_SIT_TOT_TURNO)
+  if (any(df$ANO_ELEICAO  %in% c(2018, 2022))) {
+    df_selected <- df %>%
+      select(ANO_ELEICAO, NR_TURNO, SG_UF, NM_UE, CD_MUNICIPIO,
+             NM_MUNICIPIO, DS_CARGO, NM_CANDIDATO, NM_URNA_CANDIDATO,
+             SG_PARTIDO, NM_PARTIDO, QT_VOTOS_NOMINAIS_VALIDOS,
+             DS_SIT_TOT_TURNO)
+    
+  } else {
+    
+    df_selected <- df %>%
+      select(ANO_ELEICAO, NR_TURNO, SG_UF, NM_UE, CD_MUNICIPIO,
+             NM_MUNICIPIO, DS_CARGO, NM_CANDIDATO, NM_URNA_CANDIDATO,
+             SG_PARTIDO, NM_PARTIDO, QT_VOTOS_NOMINAIS,
+             DS_SIT_TOT_TURNO)
+  }
   
-  return(df)
+  return(df_selected)
 }
 
 
@@ -87,14 +106,18 @@ adjust_abbreviation_parties <- function(df) {
   df$SG_PARTIDO[df$SG_PARTIDO == "PHS"] <- "PODEMOS"
   df$SG_PARTIDO[df$SG_PARTIDO == "PMDB"] <- "MDB"
   df$SG_PARTIDO[df$SG_PARTIDO == "PC do B"] <- "PCdoB"
-  df$SG_PARTIDO[df$SG_PARTIDO == "PPS"] <- "CIDADANIA"
+  df$SG_PARTIDO[df$SG_PARTIDO == "PPL"] <- "PCdoB"
+  df$SG_PARTIDO[df$SG_PARTIDO == "PPS"] <- "Cidadania"
   df$SG_PARTIDO[df$SG_PARTIDO == "PR"] <- "PL"
   df$SG_PARTIDO[df$SG_PARTIDO == "PRB"] <- "REPUBLICANOS"
   df$SG_PARTIDO[df$SG_PARTIDO == "PTC"] <- "AGIR"
+  df$SG_PARTIDO[df$SG_PARTIDO == "PEN"] <- "Patriota"
+  df$SG_PARTIDO[df$SG_PARTIDO == "PT do B"] <- "AVANTE"
   
   df$SG_PARTIDO[df$SG_PARTIDO == "REPUBLICANOS"] <- "Republicanos"
   df$SG_PARTIDO[df$SG_PARTIDO == "UNIÃO BRASIL"] <- "União Brasil"
   df$SG_PARTIDO[df$SG_PARTIDO == "PATRIOTA"] <- "Patriota"
+  df$SG_PARTIDO[df$SG_PARTIDO == "PATRI"] <- "Patriota"
   df$SG_PARTIDO[df$SG_PARTIDO == "SOLIDARIEDADE"] <- "Solidariedade"
   
   return(df)
@@ -165,42 +188,57 @@ set_proportion_by_year_party <- function(df) {
 
 proportional_stacked_area <- function(df) {
   
-  df$SG_PARTIDO <- factor(df$SG_PARTIDO,
-                          levels=c(
-                            "PMN", "PSOL", "PCdoB", "PT", "PV", "PDT", "PSB", 
-                            "Solidariedade", "MDB", "PSD", "PSDB", "AVANTE", "PRP",
-                            "PROS", "PP", "PTC", "PRTB", "Patriota", "PHS",
-                            "PSC", "União Brasil", "PL", "PTB", "Republicanos"
-                          )
-  )
+  # df$SG_PARTIDO <- factor(df$SG_PARTIDO,
+  #                         levels=c(
+  #                           "PMN", "PSOL", "PCdoB", "PT", "PV", "PDT", "PSB", 
+  #                           "Solidariedade", "MDB", "PSD", "PSDB", "AVANTE", "PRP",
+  #                           "PROS", "PP", "PTC", "PRTB", "Patriota", "PHS",
+  #                           "PSC", "União Brasil", "PL", "PTB", "Republicanos"
+  #                         )
+  # )
   
   # Proportional Stacked Area Chart
-  df %>%
+   plot <- df %>%
     ggplot(aes(x= ANO_ELEICAO,
-               y= PROPORTION,  # REQUIRE TO DO
+               y= PROPORTION,
                fill=SG_PARTIDO)) +
-    geom_area(alpha=0.6 , 
-              size=0.7, 
-              colour="black") +
-    scale_fill_manual(values = parties_colores)
+    geom_area(alpha=0.5) +
+    theme(
+      plot.margin = margin(20, 20, 20, 20),
+      plot.title = element_text(vjust=3, hjust=.5),
+      panel.background = element_blank(),
+      panel.grid.major = element_line(colour = "grey88", linetype='dashed'),
+      panel.grid.minor = element_blank(),
+      axis.title.x = element_text(vjust=-2),
+      axis.title.y = element_text(vjust=3),
+      axis.ticks = element_blank(),
+      legend.title = element_blank()
+    ) +
+    scale_fill_manual(values = parties_colores) +
+    scale_x_continuous(breaks = c(2010, 2014, 2018, 2022)) +
+    labs(title="Variation of parties in Minas Gerais",
+      y="Proportion",
+           x="Election Year")
   
+  return(plot)
 }
 
 
 
 # ~~~~ function ~~~~ SAVE PLOT
 
-plot_save <- function(df) {
+plot_save <- function(plot, df) {
   
-  if ("Deputado Federal" %in% df$DS_CARGO) {
+  if (df$DS_CARGO %in% c("Deputado Federal", "DEPUTADO FEDERAL")) { # zero length  !!!!!!!!!!!!!!!
     rep_type <- "federal"
+    
   } else {
     rep_type <- "state"
   }
   
   file_name <- paste0("../saved_charts/proportional_stacked_area_MG_representatives_", rep_type, ".jpg")
   
-  ggsave(df, filename=file_name,
+  ggsave(plot, filename=file_name,
          units = "cm",
          width = 15,
          height = 10
@@ -224,9 +262,6 @@ automatic_analysis <-function(df_list) {
     state_rep <- select_elected(df_representatives[[1]])
     federal_rep <- select_elected(df_representatives[[2]])
     
-    print(typeof(state_rep))
-    print(dim(state_rep))
-    
     state_rep$QUANTITY <- 1
     federal_rep$QUANTITY <- 1
     
@@ -237,11 +272,11 @@ automatic_analysis <-function(df_list) {
     by_federals <- rbind(by_federals, federal_rep_by_party)
   }
   
-  plot_state_rep <- proportional_stacked_area(by_states)
+  plot_state_rep <-  proportional_stacked_area(by_states)
   plot_federals_rep <- proportional_stacked_area(by_federals)
   
-  plot_save(plot_state_rep)
-  plot_save(plot_federals_rep)
+  plot_save(plot_state_rep, by_states)
+  plot_save(plot_federals_rep, by_federals)
   
 }
 
@@ -253,22 +288,22 @@ automatic_analysis <-function(df_list) {
 
 # ~~~~~~~~~~~~~~~~  IMPORT DATA  ~~~~~~~~~~~~~~~~
 
-#mg_2010 <- read.table("../dados/***************", header=TRUE, sep=";")
-#mg_2014 <- read.table("../dados/***************", header=TRUE, sep=";")
+mg_2010 <- read.table("../dados/votacao_candidato_munzona_2010_MG.csv", header=TRUE, sep=";")
+mg_2014 <- read.table("../dados/votacao_candidato_munzona_2014_MG.csv", header=TRUE, sep=";")
 mg_2018 <- read.table("../dados/votacao_candidato_munzona_2018_MG.csv", header=TRUE, sep=";")
 mg_2022 <- read.table("../dados/votacao_candidato_munzona_2022_MG.csv", header=TRUE, sep=";")
 
 all_dataframes <- list(
   mg_2022,
-  mg_2018
+  mg_2018,
+  mg_2014,
+  mg_2010
 )
-
 
 
 
 # ~~~~~~~~~~~~~~~~  AUTOMATION  ~~~~~~~~~~~~~~~~
 
 automatic_analysis(all_dataframes)
-
 
 
---
+++ ready_code/semicircle_parliament_composition_MG.R
@@ -0,0 +1,239 @@
+# SEMICIRCLE PARLIAMENT COMPOSITION
+
+# TO IMPROVE: FEDERAL PARLIAMENT SEMICIRCLE require reduce space between dots
+# TO IMPROVE: change number of rows for federal and state parliaments
+# TO IMPROVE: set ORDER to party by ideological alignment
+
+
+
+# SET MAIN DIRECTORY
+setwd("C:/Users/guima/Desktop/data_science/Projetos/elections_analytics_-_minas_gerais/in_progress")
+
+
+library(dplyr)
+library(ggplot2)
+library(cowplot)
+library(ggparliament)
+
+
+
+
+
+# ~~~~~~~~~~~~~~~~  FUNCTIONS  ~~~~~~~~~~~~~~~~
+
+# ~~~~ function ~~~~ SELECT ONLY ELECTED
+
+select_elected <- function(df) {
+  
+  situation <- c(
+    "ELEITO ELEITO POR MÉDIA",
+    "ELEITO POR QP",
+    "ELEITO"
+  )
+  
+  df_elected <- df %>%
+    dplyr::filter(DS_SIT_TOT_TURNO %in% situation)
+  
+  return(df_elected)
+}
+
+
+
+# ~~~~ function ~~~~ change PARTIES NAMES  ~~~~~~~~~~~~~~~~
+
+adjust_abbreviation_parties <- function(df) {
+  
+  df$SG_PARTIDO[df$SG_PARTIDO == "PFL"] <- "UNIÃO BRASIL"
+  df$SG_PARTIDO[df$SG_PARTIDO == "DEM"] <- "UNIÃO BRASIL"
+  df$SG_PARTIDO[df$SG_PARTIDO == "PSL"] <- "UNIÃO BRASIL"
+  df$SG_PARTIDO[df$SG_PARTIDO == "UNIÃO"] <- "UNIÃO BRASIL"
+  df$SG_PARTIDO[df$SG_PARTIDO == "PODE"] <- "PODEMOS"
+  df$SG_PARTIDO[df$SG_PARTIDO == "PTN"] <- "PODEMOS"
+  df$SG_PARTIDO[df$SG_PARTIDO == "PHS"] <- "PODEMOS"
+  df$SG_PARTIDO[df$SG_PARTIDO == "PMDB"] <- "MDB"
+  df$SG_PARTIDO[df$SG_PARTIDO == "PC do B"] <- "PCdoB"
+  df$SG_PARTIDO[df$SG_PARTIDO == "PPL"] <- "PCdoB"
+  df$SG_PARTIDO[df$SG_PARTIDO == "PPS"] <- "Cidadania"
+  df$SG_PARTIDO[df$SG_PARTIDO == "PR"] <- "PL"
+  df$SG_PARTIDO[df$SG_PARTIDO == "PRB"] <- "REPUBLICANOS"
+  df$SG_PARTIDO[df$SG_PARTIDO == "PTC"] <- "AGIR"
+  df$SG_PARTIDO[df$SG_PARTIDO == "PEN"] <- "Patriota"
+  df$SG_PARTIDO[df$SG_PARTIDO == "PSDC"] <- "DC"
+  df$SG_PARTIDO[df$SG_PARTIDO == "PT do B"] <- "AVANTE"
+  
+  df$SG_PARTIDO[df$SG_PARTIDO == "REPUBLICANOS"] <- "Republicanos"
+  df$SG_PARTIDO[df$SG_PARTIDO == "UNIÃO BRASIL"] <- "União Brasil"
+  df$SG_PARTIDO[df$SG_PARTIDO == "PATRIOTA"] <- "Patriota"
+  df$SG_PARTIDO[df$SG_PARTIDO == "PATRI"] <- "Patriota"
+  df$SG_PARTIDO[df$SG_PARTIDO == "SOLIDARIEDADE"] <- "Solidariedade"
+  
+  return(df)
+}
+
+
+
+# ~~~~ function ~~~~ FILTER STATE AND FEDERAL
+
+select_federal_and_state <- function(df) {
+  
+  df_state <- df %>%
+    filter(DS_CARGO %in% c("Deputado Estadual", "DEPUTADO ESTADUAL"))
+  
+  df_federal <- df %>%
+    filter(DS_CARGO %in% c("Deputado Federal", "DEPUTADO FEDERAL"))
+  
+  df_state_federal <- list(df_federal, df_state)
+  
+  return(df_state_federal)
+}
+
+
+
+# ~~~~ function ~~~~ reduce variables and set seats
+
+reduce_var_set_seats <- function(df) {
+  
+  elected_grouped <- df %>%
+    group_by(NM_CANDIDATO, SG_PARTIDO, DS_SIT_TOT_TURNO) %>%
+    summarise(QT_VOTOS_NOMINAIS_VALIDOS = sum(QT_VOTOS_NOMINAIS_VALIDOS))
+  
+  elected_grouped$SEATS <- 1
+  
+  grouped_df <- elected_grouped %>%
+    group_by(SG_PARTIDO) %>%
+    summarise(SEATS = sum(SEATS))
+  
+  return(grouped_df)
+}
+
+
+
+# ~~~~ function ~~~~ PLOT
+
+generate_plot <- function(df) {
+  
+  parliament_data <- parliament_data(
+    election_data = df,
+    type = "semicircle",
+    party_seats = df$SEATS,
+    parl_rows=3,
+    plot_order = df$SEATS,
+  )
+  
+  plot <- parliament_data %>%
+    ggplot(aes(x, y, colour= SG_PARTIDO)) +
+    geom_parliament_seats(size = 3.5) +
+    theme_ggparliament() +
+    theme(
+      plot.margin = margin(1, 1, .3, 1, "cm"),
+      plot.title = element_text(margin = margin(0, 0, 30, 0)),
+      legend.title = element_blank(),
+      legend.position = "bottom",
+      legend.margin=margin(.5, 0, 0, 0, unit='cm')
+    ) +
+    scale_colour_manual(values = parties_colores_2022) +
+    labs(title = "Composição do Legislativo")
+  
+  return(plot)
+}
+
+
+
+# ~~~~ function ~~~~ SAVE
+
+save_plot <- function(plot, df) {
+  
+  if (any(df$DS_CARGO %in% c("Deputado Federal", "DEPUTADO FEDERAL"))) { # zero length  !!!!!!!!!!!!!!!
+    rep_type <- "federal"
+    
+  } else {
+    rep_type <- "state"
+  }
+  
+  file_name <- paste0("../saved_charts/semicircle_parliamment_composition_MG_representatives_", rep_type, ".jpg")
+  
+  ggsave(plot, filename=file_name,
+         units = "cm",
+         width = 15,
+         height = 13
+  )
+}
+
+
+
+# ~~~~ function ~~~~ AUTOMATION
+
+automatic_analysis <- function(df) {
+  
+  # ~~~~~  SELECT only ELECTEDS  ~~~~~
+  electeds <- select_elected(df)
+  
+  # ~~~~~  ADJUST PARTY NAMES  ~~~~~
+  electeds_adjusted <- adjust_abbreviation_parties(electeds)
+  
+  # ~~~~~  SELECT ELECTEDS FEDERAL AND STATE DEPUTIES  ~~~~~
+  df_federals_and_states <- select_federal_and_state(electeds_adjusted) # return list (federal, state)
+  
+  # ~~~~~  SELECT ELECTEDS FEDERAL AND STATE DEPUTIES  ~~~~~
+  by_federal <- reduce_var_set_seats(df_federals_and_states[[1]])
+  by_state <- reduce_var_set_seats(df_federals_and_states[[2]])
+  
+  # ~~~~~  PLOT  ~~~~~
+  plot_federals <- generate_plot(by_federal)
+  plot_state <- generate_plot(by_state)
+  
+  # ~~~~~  SAVE  ~~~~~
+  save_plot(plot_federals, df_federals_and_states[[1]])
+  save_plot(plot_state, df_federals_and_states[[2]])
+}
+
+
+
+
+
+# ~~~~~~~~~~~~~~~~  IMPORT DATA  ~~~~~~~~~~~~~~~~
+
+#mg_2010 <- read.table("../dados/votacao_candidato_munzona_2010_MG.csv", header=TRUE, sep=";")
+#mg_2014 <- read.table("../dados/votacao_candidato_munzona_2014_MG.csv", header=TRUE, sep=";")
+#mg_2018 <- read.table("../dados/votacao_candidato_munzona_2018_MG.csv", header=TRUE, sep=";")
+mg_2022 <- read.table("../dados/votacao_candidato_munzona_2022_MG.csv", header=TRUE, sep=";")
+
+
+
+
+# ~~~~~~~~~~~~~~~~  GENERAL VARIABLES  ~~~~~~~~~~~~~~~~
+# TO IMPROVE: create a loop to check parties on the dataframe
+
+parties_colores_2022 <- c(
+  "PMN"="tomato4",
+  "PSOL"="orange",
+  "PV"="green",
+  "PT"="red",
+  "PDT"="firebrick",
+  "PSB"="gold",
+  "Rede"="darkseagreen",
+  "Solidariedade"="darkorange4",
+  "MDB"="grey",
+  "PSD"="aquamarine1",
+  "Cidadania"="darkorchid1",
+  "PSDB"="royalblue",
+  "AVANTE"="brown1",
+  "PROS"="tan1",
+  "PP"="tan4",
+  "Patriota"="darkolivegreen1",
+  "PSC"="cyan",
+  "União Brasil"="deepskyblue",
+  "NOVO"="darkorange",
+  "PL"="darkblue",
+  "Republicanos"="mediumblue",
+  "DC"="dodgerblue"
+)
+
+
+
+
+# ~~~~~~~~~~~~~~~~  AUTOMATION  ~~~~~~~~~~~~~~~~
+
+automatic_analysis(mg_2022)
+
+
